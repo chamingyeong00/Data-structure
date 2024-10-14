31cffaa68538985301e37620b647f30b61882c62
ChaMinGyeong <mingyeong1104@naver.com>
2024-10-15 03:17:46 +0900

    add 4part stack_linklist.c

--- 4part/stack_linklist.c
+++ 4part/stack_linklist.c
@@ -12,5 +12,28 @@ top = NULL;
 
 void push(element item)
 {
-    
+    struct stack *temp= (struct stack*)malloc(sizeof(struct stack));
+    if(temp == NULL)
+    {
+        fprintf(stderr, "The memory is full\n");
+        exit(1);
+    }
+    temp->data = item;
+    temp->link = top; //stack이기때문에
+    top = temp;
+}
+
+element pop()
+{
+    struct stack *temp= top;
+    element item;//
+    if(temp == NULL)
+    {
+        fprintf(stderr, "The memory is full\n");
+        exit(1);
+    }
+    item = temp->data
+    top = temp->link;//temp->link
+    free(temp);
+    return item;
 }
